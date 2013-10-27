e113a6048452343a462292cdf325056adc9a1f51
Andrew Burton <tuglyraisin@gmail.com>
2013-10-27 16:26:35 -0500

    Started porting some functions.

--- TemplateEngine/HtmlRender.cs
+++ TemplateEngine/HtmlRender.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,26 @@ namespace TemplateEngine
 {
     class HtmlRender
     {
+        public void AddVariable(string key, string val)
+        {
+        }
+
+        public void RemoveVariable(string key)
+        {
+        }
+
+        public HtmlRender() { }
+
+        public HtmlRender(Hashtable pre_var)
+        {
+        }
+
+        // PRIVATE
+        private string Lz(string n)
+        {
+            int i = int.Parse(n);
+            if (i < 10) { return "0" + n; }
+            return n;
+        }
     }
 }
