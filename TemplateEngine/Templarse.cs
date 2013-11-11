c1b293b8c5429971702590ff01d868452cd1fa18
Andrew Burton <tuglyraisin@gmail.com>
2013-11-11 08:13:56 -0600

    Working on Templarse.cs before breakfast.

--- TemplateEngine/Templarse.cs
+++ TemplateEngine/Templarse.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -8,6 +9,26 @@ namespace TemplateEngine
 {
     class Templarse
     {
+        // PROPERTIES
+        private string _template = "";
+        public string Template
+        {
+            get { return _template; }
+        }
+
+        private List<StackItem> _stack;
+        public List<StackItem> Stack
+        {
+            get { return _stack; }
+        }
+
+        private Hashtable _commands;
+        public Hashtable Commands
+        {
+            get { return _commands; }
+        }
+
+        // PUBLIC
         public string ExecuteTemplate()
         {
             string retval = "";
@@ -17,8 +38,20 @@ namespace TemplateEngine
             return retval;
         }
 
+        // PRIVATE
+        private List<StackItem> ParseTemplate(string html)
+        {
+            List<StackItem> retval = new List<StackItem>();
+
+            return retval;
+        }
+
+        // CONSTRUCTOR
         public Templarse(string html, Hashtable cmds)
         {
+            _template = html;
+            
+
         }
     }
 }
