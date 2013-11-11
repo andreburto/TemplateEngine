c6be692853df59325830dee1ce09a0e3009ee653
Andrew Burton <tuglyraisin@gmail.com>
2013-11-11 12:06:46 -0600

    More work on Templarse.cs after breakfast.

--- TemplateEngine/Templarse.cs
+++ TemplateEngine/Templarse.cs
@@ -32,17 +32,47 @@ namespace TemplateEngine
         public string ExecuteTemplate()
         {
             string retval = "";
-
-            // code goes here
-
+            foreach (StackItem i in _stack)
+            {
+                if (i.Type.Equals(StackItem.ItemType.HTML))
+                {
+                    retval += i.Item;
+                }
+                else if (i.Type.Equals(StackItem.ItemType.COMMAND))
+                {
+                    retval += Commands(i.Item);
+                }
+            }
             return retval;
+        }
+
+        public string Commands(string c)
+        {
+            string[] parts = c.Split(' ');
+            string cmd = parts[0].ToLower();
+
+            // If the first command isn't a command return blank
+            if (_commands.Contains(cmd) == false) { return ""; }
+
+            // Copies the command table except for the current command
+            Hashtable temp_cmds = new Hashtable();
+            foreach (string k in _commands.Keys)
+            {
+                if (k != cmd) { temp_cmds[k] = _commands[k]; }
+            }
+
+            Templarse temp_tp = new Templarse(this.Commands(cmd), temp_cmds);
+            temp_cmds = null;
+
+            // Return
+            return temp_tp.ExecuteTemplate();
         }
 
         // PRIVATE
         private List<StackItem> ParseTemplate(string html)
         {
             List<StackItem> retval = new List<StackItem>();
-
+            
             return retval;
         }
 
@@ -50,8 +80,8 @@ namespace TemplateEngine
         public Templarse(string html, Hashtable cmds)
         {
             _template = html;
-            
-
+            _stack = ParseTemplate(html);
+            _commands = cmds;
         }
     }
 }
